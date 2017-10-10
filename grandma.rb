33b3b2f149aa50cd8b6b45e02343caf4939374f5
Kayla Hansen <shiverr@gmail.com>
2017-10-10 01:58:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 
 def speak_to_grandma(string)
- if (string == string.upcase)
+if (string == "I LOVE YOU GRANDMA!")
+  return "I LOVE YOU TOO PUMPKIN!"
+elsif (string == string.upcase)
    return "NO, NOT SINCE 1938!"
- elsif (string == "I LOVE YOU GRANDMA!")
-   return "I LOVE YOU TOO PUMPKIN!"
  else
    return "HUH?! SPEAK UP, SONNY!"
 end
